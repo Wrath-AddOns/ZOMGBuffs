33d98424149cacf22500bdc890f6c3ba7e9f5d47
dessa <dessa@3e8d1ccc-f021-4586-b1a9-bb403ccde2e2>
2009-04-23 17:39:24 +0000

    first pass of porting the addon to the wowace localization app, or: lets see if i managed to break something...

---
+++ trunk/ZOMGBuffs_Log/Locales/localization.koKR.lua
@@ -0,0 +1,5 @@
+﻿local L = LibStub("AceLocale-2.2"):new("ZOMGLog")
+
+L:RegisterTranslations("koKR", function() return {
+--@localization(locale="koKR", format="lua_table", namespace="ZOMGLog")@
+} end)
---
+++ trunk/ZOMGBuffs_Log/Locales/localization.ruRU.lua
@@ -0,0 +1,5 @@
+﻿local L = LibStub("AceLocale-2.2"):new("ZOMGLog")
+
+L:RegisterTranslations("ruRU", function() return {
+--@localization(locale="ruRU", format="lua_table", namespace="ZOMGLog")@
+} end)
