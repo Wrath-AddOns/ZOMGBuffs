f41fe0d48c42869d96a7610746d8bf4e548e3110
dessa <dessa@3e8d1ccc-f021-4586-b1a9-bb403ccde2e2>
2009-04-23 18:11:55 +0000

    fix my locale breakage, hopefully, bah.

---
+++ trunk/ZOMGBuffs_Log/Locales/localization.deDE.lua
@@ -0,0 +1,5 @@
+﻿local L = LibStub("AceLocale-2.2"):new("ZOMGLog")
+
+L:RegisterTranslations("deDE", function() return
+--@localization(locale="deDE", format="lua_table", namespace="ZOMGLog")@
+end)
--- trunk/ZOMGBuffs_Log/Locales/localization.zhTW.lua
+++ trunk/ZOMGBuffs_Log/Locales/localization.zhTW.lua
@@ -1,5 +1,5 @@
 ﻿local L = LibStub("AceLocale-2.2"):new("ZOMGLog")
 
-L:RegisterTranslations("zhTW", function() return {
+L:RegisterTranslations("zhTW", function() return
 --@localization(locale="zhTW", format="lua_table", namespace="ZOMGLog")@
-} end)
+end)
