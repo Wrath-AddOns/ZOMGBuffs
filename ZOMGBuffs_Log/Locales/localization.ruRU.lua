84cb9532e5aa90a19e5b574e28f8003e45a04cdd
Zeksie <Zeksie@3e8d1ccc-f021-4586-b1a9-bb403ccde2e2>
2009-04-26 16:17:44 +0000

    ZOMGBuffs: Fix missing&broken locales in Log module.

--- ZOMGBuffs_Log/Locales/localization.ruRU.lua
+++ ZOMGBuffs_Log/Locales/localization.ruRU.lua
@@ -1,5 +1,5 @@
 ﻿local L = LibStub("AceLocale-2.2"):new("ZOMGLog")
 
-L:RegisterTranslations("ruRU", function() return
+L:RegisterTranslations("ruRU", function() return {
 --@localization(locale="ruRU", format="lua_table", namespace="ZOMGLog")@
-end)
+} end)
